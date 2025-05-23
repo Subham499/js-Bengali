beb0f80265d7afc3d84bb8cfc6dfa3313ba17854
Subham Chakraborty <subhamchakraborty353@gmail.com>
2025-05-23 16:47:04 +0000

    cOnversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+let age = 44 // when we print this
+console.log(typeof age); //this is show number
+
+let againAge ="44"// when we print this 
+console.log(typeof againAge);// this is show string
+
+let againAge1="44abc"//when we print this
+console.log(typeof againAge1);// this is show NaN
+
+let valueInNumber= Number(age) // when we prtint with this it always show number if it is string,in js 
+                               // we convert string into number or etc
+console.log(typeof valueInNumber);
+
+let age1=null
+console.log(typeof age1);// this show object or 0
+
+let age2;
+console.log(typeof age2);//it is show undefined
+
+//"33"=>33
+// "33abc" =>NaN
+//true =>1; false => 0 
+
+let isLoggedIn= 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false 
+// ""=> flase
+// "subham" => true
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);//it will show 33
+
+console.log(typeof stringNumber);//but it will show that it is a string
+
+
+
+
+
+
+
+
+
