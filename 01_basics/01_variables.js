69cd4d9c0267e9090e778fd92dcb2a1a58e8401f
Subham Chakraborty <subhamchakraborty353@gmail.com>
2025-05-23 15:52:52 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1445532
+let accountEmail = "subham@gmail.com"
+var accountPasswoard = "12345"
+accountCity = "Burdwan"
+let accountState;
+
+//accountId = 3 not allowed
+
+accountEmail="hc@gmail.com"
+accountPassword="12788"
+accountCity="kolkata"
+
+
+console.log(accountId);
+
+/*
+ prefer not use var because of block scope
+*/
+
+console.table([accountId, accountCity,accountPassword,accountEmail,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code in a newer version
+//alert(2+2) we are using node js not browser
+ // datatypes
+ let name="subham"//string datatypes
+ let age="21"//intger
+ let isLoggedIn= false //boolean
+
+ //premetive datatypes
+
+// number =>2 to power 53
+//bigint it is use for very big intger numbers
+// strings=>""
+//boolean => yes or no
+//null => stand alone value
+//undefine => not asign value
+//symbol => unique
+
+console.log(typeof null);// type of object
+console.log(typeof undefined);// type of undefined
+
